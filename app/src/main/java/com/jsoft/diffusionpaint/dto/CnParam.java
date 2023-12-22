7bdb58f22a04263985081c367d3207a70f639cd7
Jorden Tse <jordenyt@gmail.com>
2023-12-22 14:51:46 +0800

    Add cnResizeMode as JSON param

--- app/src/main/java/com/jsoft/diffusionpaint/dto/CnParam.java
+++ app/src/main/java/com/jsoft/diffusionpaint/dto/CnParam.java
@@ -5,8 +5,13 @@ public class CnParam {
     public String cnInputImage; //Background, Sketch
     public String cnModule;
     public String cnModelKey;
+    public int cnResizeMode = -1;
     public int cnControlMode;
     public double cnWeight;
     public double cnModuleParamA;
     public double cnModuleParamB;
+
+    public static final String CN_RESIZE_MODE_RESIZE = "Just Resize";
+    public static final String CN_RESIZE_MODE_CROP = "Crop and Resize";
+    public static final String CN_RESIZE_MODE_FILL = "Resize and Fill";
 }
