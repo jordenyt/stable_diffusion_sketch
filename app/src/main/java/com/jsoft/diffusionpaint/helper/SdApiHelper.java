1d23d97ada936c123f1303bc85a166c6971c4cbd
Jorden Tse <jordenyt@gmail.com>
2024-06-20 11:12:58 +0800

    update for maskBlur

--- app/src/main/java/com/jsoft/diffusionpaint/helper/SdApiHelper.java
+++ app/src/main/java/com/jsoft/diffusionpaint/helper/SdApiHelper.java
@@ -166,8 +166,7 @@ public class SdApiHelper {
             Bitmap baseImage = Utils.extractBitmap(mCurrentSketch.getImgBackground(), inpaintArea);
             jsonObject.put("background", Utils.jpg2Base64String(baseImage));
             float ratio = max(inpaintArea.width(), inpaintArea.height()) / sdParam.sdSize;
-            int boundary = Integer.parseInt(sharedPreferences.getString("inpaintMaskBlur", "10"));
-            mCurrentSketch.setImgInpaintMask(Sketch.getInpaintMaskFromPaint(mCurrentSketch, round(boundary * ratio), true));
+            mCurrentSketch.setImgInpaintMask(Sketch.getInpaintMaskFromPaint(mCurrentSketch, round(sdParam.maskBlur * ratio), true));
             Bitmap paintImage = Utils.extractBitmap(mCurrentSketch.getImgInpaintMask(), inpaintArea);
             jsonObject.put("paint", Utils.jpg2Base64String(paintImage));
             jsonObject.put("reference", Utils.jpg2Base64String(mCurrentSketch.getImgReference()));
