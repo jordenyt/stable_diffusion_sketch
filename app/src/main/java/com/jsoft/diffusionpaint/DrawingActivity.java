05800bf3495e8d9cd984fd95a822386263baf1e4
Jorden Tse <jordenyt@gmail.com>
2024-08-26 16:28:49 +0800

    Revert "Merge branch 'main' into comfyui"
    This reverts commit 6280f6fa9cac1e19d7535f814f884b0331ba4c4e, reversing
    changes made to 1a1b54dd1e25c8ec2e3c4970d4a49859cb859a9d.

--- app/src/main/java/com/jsoft/diffusionpaint/DrawingActivity.java
+++ app/src/main/java/com/jsoft/diffusionpaint/DrawingActivity.java
@@ -71,6 +71,7 @@ public class DrawingActivity extends AppCompatActivity implements ColorPickerDia
     private SeekBar seekBlur;
     private TextView tvWidth;
     private TextView tvBlur;
+    private TextView txtVRAM;
     private String aspectRatio;
     private SdApiHelper sdApiHelper;
     FloatingActionButton paletteButton;
@@ -577,6 +578,9 @@ public class DrawingActivity extends AppCompatActivity implements ColorPickerDia
 
         AlertDialog dialog = builder.create();
         if(!isFinishing()) dialog.show();
+
+        txtVRAM = dialogView.findViewById(R.id.sd_vram_txt);
+        sdApiHelper.sendGetRequest("getVRAM", "/sdapi/v1/memory");
     }
 
     ActivityResultLauncher<Intent> sdViewerActivityResultLauncher = registerForActivityResult(
@@ -728,6 +732,14 @@ public class DrawingActivity extends AppCompatActivity implements ColorPickerDia
             try {
                 JSONObject jsonObject = new JSONObject(responseBody);
                 promptTextView.setText(jsonObject.getString("caption"));
+            } catch (JSONException ignored) {}
+        } else if ("getVRAM".equals(requestType)) {
+            try {
+                JSONObject jsonObject = new JSONObject(responseBody);
+                String message = "";
+                message += (Math.round(jsonObject.getJSONObject("cuda").getJSONObject("system").getDouble("used") / 1024d / 1024d / 1024d * 10) / 10d) + "GB" + "/";
+                message += (Math.round(jsonObject.getJSONObject("cuda").getJSONObject("system").getDouble("total") / 1024d / 1024d / 1024d * 10) / 10d) + "GB";
+                txtVRAM.setText(message);
             } catch (JSONException ignored) {}
         }
     }
